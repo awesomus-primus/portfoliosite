44373476f2480d6fef942fcb4ad9eeb16fe72e8c
Yordan Angelov <yordan.angelov@centre4.local>
2017-11-02 12:13:22 +0000

    Attempting to create a toggling navbar

--- script.js
+++ script.js
@@ -1,10 +1,66 @@
 $(document).ready(function () {
+
     $(".flip").flip({
         trigger: 'hover'
     });
-});
 
-$(document).ready(function(){
-    $('#about').animate({ 
-        left: '80%' ,}, "slow");
+    $('#intro').animate({
+        left: '80%',
+    }, "slow");
+
+    $(window).scroll(function () {
+        var hT = $('#contact').offset().top,
+            hH = $('#contact').outerHeight(),
+            wH = $(window).height(),
+            wS = $(this).scrollTop();
+        if (wS > (hT + hH - wH)) {
+            $('#contact').hide();
+            $('#contact').animate({
+                right: '80%',
+            }, "slow");
+            $('#contact').show();
+        }
+    });
+
+    $(window).scroll(function () {
+        var hT = $('#portfolio').offset().top,
+            hH = $('#portfolio').outerHeight(),
+            wH = $(window).height(),
+            wS = $(this).scrollTop();
+        if (wS > (hT + hH - wH)) {
+            $('#portfolio').animate({
+                left: '80%',
+            }, "slow");
+        }
+    });
+
+    $(window).scroll(function () {
+        var hT = $('#areas').offset().top,
+            hH = $('#areas').outerHeight(),
+            wH = $(window).height(),
+            wS = $(this).scrollTop();
+        if (wS > (hT + hH - wH)) {
+            $('#areas').animate({
+                right: '80%',
+            }, "slow");
+        }
+    });
+
+    var navbarToggle = document.getElementById("navbar-toggle-wrapper");
+    var navbarMenu = document.getElementById("nav-menu");
+
+    function toggleNavMenu(){
+        navbarToggle.classList.toggle("active");
+
+        toggleNav();
+    }
+
+    function toggleNav(){
+        navbarMenu.classList.toggle("active");
+    }
+
+    document.addEventListener("DOMContentLoaded", function(){
+        navbarToggle.addEventListener("click", function() {toggleNavMenu();});
+    });
+
 });
