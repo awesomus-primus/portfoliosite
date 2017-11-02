3c3b5354262afe6d6df6ea9f85ab4edae2d09cc6
Yordan Angelov <yordan.angelov@centre4.local>
2017-11-02 15:19:07 +0000

    Added smooth scrolling to hrefs

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ $(document).ready(function () {
     var navbarToggleClose = document.getElementById("navbar-toggle-x");
 
     navbarToggle.addEventListener("click", toggleMenu);
-    
+
     function toggleMenu() {
         if (navbarMenu.style.display === "none") {
             navbarMenu.style.display = "inline";
@@ -68,8 +68,34 @@ $(document).ready(function () {
             navbarToggleIcon.style.display = "block";
             navbarToggleClose.style.display = "none";
         }
-        
     }
+
+    // Smooth scrolling javascript
+    $('a[href*="#"]').not('[href="#"]').not('[href="#0"]').click(function (event) {
+        if (
+            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+            &&
+            location.hostname == this.hostname
+        ) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000, function () {
+                    var $target = $(target);
+                    $target.focus();
+                    if ($target.is(":focus")) {
+                        return false;
+                    } else {
+                        $target.attr('tabindex', '-1');
+                        $target.focus();
+                    };
+                });
+            }
+        }
+    });
 
 });
 
